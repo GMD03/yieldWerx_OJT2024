f118976eaef4877cd2f74df8387542cafe5e320b
GMD03 <ghiamcmahon@gmail.com>
2024-08-12 07:28:05 +0800

    range update for scatter plot(Unsure)

--- views/selection_criteria.php
+++ views/selection_criteria.php
@@ -86,6 +86,48 @@
                 </ul>
             </div>
 
+            <button id="dropdownSortByButton" data-dropdown-toggle="dropdownSortby" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-auto" type="button">
+                Sort By
+                <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                </svg>
+            </button>
+
+            <!-- Dropdown menu -->
+            <div id="dropdownSortby" class="z-10 hidden w-auto h-64 overflow-y-auto bg-white divide-y divide-gray-100 rounded-lg shadow">
+                <ul class="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownSortByButton">
+                <?php foreach ($groups as $group): ?>
+                    <li>
+                        <div class="flex items-center p-2 rounded hover:bg-gray-100">
+                            <input id="checkbox-item-<?= htmlspecialchars($group) ?>" name="sort-by[]" type="radio" value="<?= htmlspecialchars($group) ?>" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
+                            <label for="checkbox-item-<?= htmlspecialchars($group) ?>" class="w-full ms-2 text-sm font-medium text-gray-900 rounded"><?= htmlspecialchars($group) ?></label>
+                        </div>
+                    </li>
+                <?php endforeach; ?>
+                </ul>
+            </div>
+
+            <button id="dropdownOrderByButton" data-dropdown-toggle="dropdownOrderBy" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 float-right" type="button">
+                Order By
+                <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                </svg>
+            </button>
+
+            <!-- Dropdown menu -->
+            <div id="dropdownOrderBy" class="z-10 hidden w-auto h-64 overflow-y-auto bg-white divide-y divide-gray-100 rounded-lg shadow">
+                <ul class="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownOrderByButton">
+                <?php foreach ($groups as $group): ?>
+                    <li>
+                        <div class="flex items-center p-2 rounded hover:bg-gray-100">
+                            <input id="checkbox-item-<?= htmlspecialchars($group) ?>" name="sort-by[]" type="radio" value="<?= htmlspecialchars($group) ?>" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
+                            <label for="checkbox-item-<?= htmlspecialchars($group) ?>" class="w-full ms-2 text-sm font-medium text-gray-900 rounded"><?= htmlspecialchars($group) ?></label>
+                        </div>
+                    </li>
+                <?php endforeach; ?>
+                </ul>
+            </div> 
+
         </div>
         
         <div class="flex w-full justify-end items-end">
